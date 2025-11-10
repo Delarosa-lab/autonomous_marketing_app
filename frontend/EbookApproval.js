b927d964b153a93c50dff2a114825bc7702c5993
Delarosa-lab <higorhz022@gmail.com>
2025-11-10 10:50:20 -0300

    Fetch and approve ebooks in EbookApproval component

--- frontend/EbookApproval.js
+++ frontend/EbookApproval.js
@@ -16,3 +16,34 @@ const EbookApproval = () => {
 };
 
 export default EbookApproval;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const EbookApproval = () => {
+  const [ebooks, setEbooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/ebooks")
+      .then(res => setEbooks(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  const aprovar = (ebook) => {
+    axios.post(`http://127.0.0.1:8000/api/ebooks/aprovar/${ebook}`)
+      .then(() => alert(`Ebook ${ebook} aprovado!`))
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div>
+      {ebooks.map((ebook, idx) => (
+        <div key={idx} className="flex items-center justify-between mb-2 p-2 border rounded">
+          <span>{ebook}</span>
+          <button onClick={() => aprovar(ebook)} className="bg-green-500 text-white px-2 py-1 rounded">Aprovar</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EbookApproval;
